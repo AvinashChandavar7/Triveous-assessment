8672504f5abae41ca58e07847ed35819d78b1d28
acinashChandavar7 <avinashChnadavar7@gmail.com>
2023-08-06 20:36:17 +0530

    add

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -10,26 +10,26 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 const Home = () => {
 
-  const [user, loading, error] = useAuthState(auth);
+  // const [user, loading, error] = useAuthState(auth);
 
 
-  useEffect(() => {
-    // console.log("user", user);
+  // useEffect(() => {
+  //   // console.log("user", user);
 
-  }, [user])
+  // }, [user])
 
 
   return (
     <div className="w-full max-w-7xl mx-auto px-8">
       <div className="flex flex-wrap -mx-2 mt-32 gap-y-8">
         <div className="w-full flex justify-center">
-          {!user ? (
+          {/* {user ? (
             <div className="max-w-md">
               <News />
             </div>
-          ) : (
-            <Login />
-          )}
+          ) : ( */}
+          <Login />
+          {/*  )} */}
         </div>
       </div>
     </div>
